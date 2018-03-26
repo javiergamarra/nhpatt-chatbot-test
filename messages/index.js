d7e1d086c787a39bbc62602d7c90777800c0c67e
Javier Gamarra <nhpatt@gmail.com>
2018-03-26 15:32:48 +0200

    production version

--- messages/index.js
+++ messages/index.js
@@ -1,20 +1,23 @@
 'use strict';
-
 const builder = require('botbuilder');
 const botbuilder_azure = require('botbuilder-azure');
 const path = require('path');
 
-let useEmulator = (process.env.NODE_ENV === 'development');
-useEmulator = true;
+const useEmulator = (process.env.NODE_ENV === 'development');
 
 const connector = useEmulator ? new builder.ChatConnector() : new botbuilder_azure.BotServiceConnector({
-	appId: process.env['MicrosoftAppId'],
-	appPassword: process.env['MicrosoftAppPassword'],
-	openIdMetadata: process.env['BotOpenIdMetadata']
+    appId: process.env['MicrosoftAppId'],
+    appPassword: process.env['MicrosoftAppPassword'],
+    openIdMetadata: process.env['BotOpenIdMetadata']
 });
+
+const tableName = 'botdata';
+const azureTableClient = new botbuilder_azure.AzureTableClient(tableName, process.env['AzureWebJobsStorage']);
+const tableStorage = new botbuilder_azure.AzureBotStorage({gzipData: false}, azureTableClient);
 
 const bot = new builder.UniversalBot(connector);
 bot.localePath(path.join(__dirname, './locale'));
+bot.set('storage', tableStorage);
 
 // Make sure you add code to validate these fields
 const luisAppId = process.env.LuisAppId;
@@ -26,39 +29,32 @@ const LuisModelUrl = 'https://' + luisAPIHostName + '/luis/v1/application?id=' +
 // Main dialog with LUIS
 const recognizer = new builder.LuisRecognizer(LuisModelUrl);
 const intents = new builder.IntentDialog({recognizers: [recognizer]})
-	.matches('Greeting', (session) => {
-		session.send('!!You reached Greeting intent, you said \'%s\'.', session.message.text);
-	})
-	.matches('Help', (session) => {
-		session.send('You reached Help intent, you said \'%s\'.', session.message.text);
-	})
-	.matches('Cancel', (session) => {
-		session.send('You reached Cancel intent, you said \'%s\'.', session.message.text);
-	})
-	/*
-	.matches('<yourIntent>')... See details at http://docs.botframework.com/builder/node/guides/understanding-natural-language/
-	*/
-	.onDefault((session) => {
-		session.send('Sorry, I did not understand \'%s\'.', session.message.text);
-	});
-
-
-// https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/9cfb77bc-f570-4d4e-a138-21748defb3af?subscription-key=f1497b363d484310830cf4fb46e71472&verbose=false&q=hi
-
-// let bot = new builder.UniversalBot(connector, function (session) {
-//     session.send("You said: %s", session.message.text);
-// });
+    .matches('Greeting', (session) => {
+        session.send('You reached Greeting intent, you said \'%s\'.', session.message.text);
+    })
+    .matches('Help', (session) => {
+        session.send('You reached Help intent, you said \'%s\'.', session.message.text);
+    })
+    .matches('Cancel', (session) => {
+        session.send('You reached Cancel intent, you said \'%s\'.', session.message.text);
+    })
+    /*
+    .matches('<yourIntent>')... See details at http://docs.botframework.com/builder/node/guides/understanding-natural-language/
+    */
+    .onDefault((session) => {
+        session.send('Sorry, I did not understand \'%s\'.', session.message.text);
+    });
 
 bot.dialog('/', intents);
 
 if (useEmulator) {
-	const restify = require('restify');
-	const server = restify.createServer();
-	server.listen(3978, function () {
-		console.log('test bot endpoint at http://localhost:3978/api/messages');
-	});
-	server.post('/api/messages', connector.listen());
+    const restify = require('restify');
+    const server = restify.createServer();
+    server.listen(3978, function () {
+        console.log('test bot endpont at http://localhost:3978/api/messages');
+    });
+    server.post('/api/messages', connector.listen());
 } else {
-	module.exports = connector.listen();
+    module.exports = connector.listen();
 }
 
