d8b766a7a2864bc36069b89b9026332e3bbe9adb
Javier Gamarra <nhpatt@gmail.com>
2018-09-19 09:12:43 +0200

    Update index.js

--- messages/index.js
+++ messages/index.js
@@ -126,7 +126,7 @@ const intents = new builder.IntentDialog({recognizers: [recognizer]})
 
             session.userData.type = results.response;
 
-            post(session, 'ddm.ddmstructure/get-structure', {'structureId': 271050})
+            post(session, 'ddm.ddmstructure/get-structure', {'structureId': 157436})
                 .then(response => {
                     const message = JSON.parse(response);
                     return JSON.parse(message.definition);
